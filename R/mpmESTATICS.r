9a7fd1515ddfbcb4acdc38cdcf1c29a49a33c7d2
muschellij2 <muschellij2@gmail.com>
2019-06-06 15:29:58 -0400

    updated

--- R/mpmESTATICS.r
+++ R/mpmESTATICS.r
@@ -306,6 +306,22 @@ medianFilterSigma <- function(obj,hsig=10,mask=NULL){
   obj
 }
 
+validate_MPMData = function(mpmdata) {
+  need_names = c("model", "nFiles", "t1Files",
+                 "maskfile", "mask",
+                 "ddata", "TE", "TR", "FA",
+                 "sdim")
+  n = names(mpmdata)
+  check = need_names %in% n
+  is_null = sapply(need_names, function(x) {
+    is.null(mpmdata[[x]])
+  })
+  if (!all(check) | any(is_null)) {
+    bad = need_names[!check | is_null]
+    bad = paste(bad, sep = ", ")
+    stop(paste0("These slots are not available: ", bad))
+  }
+}
 
 estimateESTATICS <- function (mpmdata,
                               TEScale = 100,
@@ -315,6 +331,8 @@ estimateESTATICS <- function (mpmdata,
                               L = NULL,
                               maxR2star=50,
                               verbose = TRUE) {
+
+  validate_MPMData(mpmdata)
 
   ## create the design matrix of the model
   if (mpmdata$model == 2) {

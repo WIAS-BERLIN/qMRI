f8c182731e9cc66ebda2fe82082468ebcec98d6c
JPolzehl <polzehl@wias-berlin.de>
2019-08-16 17:25:19 +0200

    adjusted Vignette

--- tests/qMRI-test.R
+++ tests/qMRI-test.R
@@ -37,36 +37,46 @@ modelMPM <- estimateESTATICS(mpm, method = "NLR", verbose=FALSE)
 #  smooth maps of ESTATICS Parameters
 #
 setCores(2, reprt = FALSE)
-modelMPMQLsp1 <- smoothESTATICS(modelMPM,
+modelMPMsp1 <- smoothESTATICS(modelMPM,
                               kstar = 16,
                               alpha = 0.004,
                               patchsize=1,
                               verbose = FALSE)
-# set mask to y==11 only yo save time
-mpm$mask[,c(1:10,12:21),] <- FALSE
-# Alternatively using Quasi-Likelihood
-sigma <- 50
-modelMPMQL <- estimateESTATICS(mpm, method = "QL",
-              sigma = array(sigma, mpm$sdim), L = 1, verbose=FALSE)
 #
 #  Compute quantitative maps (R1, R2star, PD, MT)
 #
 qMRIMaps <- calculateQI(modelMPM,
                         b1File = B1File,
                         TR2 = 3.4)
-qMRIQLSmoothedp1Maps <- calculateQI(modelMPMQLsp1,
+qMRISmoothedp1Maps <- calculateQI(modelMPMsp1,
                                     b1File = B1File,
                                     TR2 = 3.4)
 #
 #   some statistics on differences between results
 #
 qm <- extract(qMRIMaps,c("R1","R2star","MT","PD"))
-qms <- extract(qMRIQLSmoothedp1Maps,c("R1","R2star","MT","PD"))
+qms <- extract(qMRISmoothedp1Maps,c("R1","R2star","MT","PD"))
 mask <- extract(mpm,"mask")
 cat("mean of estimated quantitative maps\n",
     mean(qm$R1[mask]), mean(qm$R2star[mask]), mean(qm$MT[mask]), mean(qm$PD[mask]),"\n",
-    "mean of estimated quantitative maps\n",
+    "mean of smoothed quantitative maps\n",
     mean(qms$R1[mask]), mean(qms$R2star[mask]), mean(qms$MT[mask]), mean(qms$PD[mask]),"\n",
     "Root mean squared difference between estimated and smoothed quantitative maps\n",
     sqrt(mean((qm$R1-qms$R1)[mask]^2)), sqrt(mean((qm$R2star-qms$R2star)[mask]^2)),
     sqrt(mean((qm$MT-qms$MT)[mask]^2)), sqrt(mean((qm$PD-qms$PD)[mask]^2)),"\n")
+# set mask to y==11 only yo save time
+# reduce mask to save time, need also to adapt storage of data
+mask <- extract(mpm,"mask")
+mask[,c(1:10,12:21),] <- FALSE
+mpm <- qMRI:::setMPMmask(mpm, mask)
+# Alternatively using Quasi-Likelihood
+sigma <- 50
+modelMPMQL <- estimateESTATICS(mpm, method = "QL",
+                sigma = array(sigma, mpm$sdim), L = 1, verbose=FALSE)
+qMRIMapsQL <- calculateQI(modelMPMQL,
+                b1File = B1File,
+                TR2 = 3.4)
+mask <- extract(mpm,"mask")
+qmQL <- extract(qMRIMapsQL,c("R1","R2star","MT","PD"))
+cat("mean of estimated quantitative maps using QL\n",
+    mean(qmQL$R1[mask]), mean(qmQL$R2star[mask]), mean(qmQL$MT[mask]), mean(qmQL$PD[mask]),"\n")
